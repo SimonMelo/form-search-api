e98f86b982608115a3db1f9e2966c498aa4c69df
SimonMelo <melobielcruz@gmail.com>
2024-03-22 01:56:31 -0300

    feat: add cors

--- apiRoute.js
+++ apiRoute.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const dbConnection = require("./db")
+const cors = require('cors')
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/api/submit-form', async (req, res) => {
