ae899f42bfd28c4fa5834cf399a57550819100f9
SimonMelo <melobielcruz@gmail.com>
2024-04-03 12:26:14 -0300

    feat: add method post and improvement files

--- apiRoute.js
+++ apiRoute.js
@@ -1,31 +1,58 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require('cors');
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require('cors')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(cors());
-app.use(bodyParser.json());
+let submittedFormData = []
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.post('/api/submit-form', (req, res) => {
-    const formData = req.body;
+    const formData = req.body
 
-    const mandatoryFields = ['contact.name', 'contact.email', 'contact.phone', 'preference.immobile', 'preference.location', 'preference.price'];
-    const missingFields = mandatoryFields.filter(field => !getFieldValue(formData, field));
+    const mandatoryFields = ['contact.name', 'contact.email', 'contact.phone', 'preference.immobile', 'preference.location', 'preference.price']
+    const missingFields = mandatoryFields.filter(field => !getFieldValue(formData, field))
     
     if (missingFields.length > 0) {
-        return res.status(400).json({ error: `Os campos ${missingFields.join(', ')} são obrigatórios.` });
+        return res.status(400).json({ error: `Os campos ${missingFields.join(', ')} são obrigatórios.` })
     }
 
-    console.log('Dados recebidos:', formData);
-    res.json({ message: 'Formulário enviado com sucesso' });
-});
+    const formEntry = {
+        contact: {
+            name: formData.contact.name,
+            email: formData.contact.email,
+            phone: formData.contact.phone
+        },
+        preference: {
+            immobile: formData.preference.immobile,
+            location: formData.preference.location,
+            price: formData.preference.price
+        },
+        extra: {
+            metImmobile: formData.extra.metImmobile,
+            time: formData.extra.time,
+            addInfo: formData.extra.addInfo
+          }
+        
+    }
+
+    submittedFormData.push(formEntry)
+
+    console.log('Dados recebidos:', formEntry)
+    res.json({ message: 'Formulário enviado com sucesso' })
+})
+
+app.get('/api/get-form-data', (req, res) => {
+    res.json(submittedFormData)
+})
 
 function getFieldValue(obj, fieldPath) {
-    return fieldPath.split('.').reduce((acc, curr) => acc[curr], obj);
+    return fieldPath.split('.').reduce((acc, curr) => acc[curr], obj)
 }
 
 app.listen(port, () => {
-    console.log(`Servidor está rodando em http://localhost:${port}`);
-});
+    console.log(`Servidor está rodando em http://localhost:${port}`)
+})
