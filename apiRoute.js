17de24d44e5c0140e0e9bb75b1d16de419b6e332
SimonMelo <melobielcruz@gmail.com>
2024-04-09 15:56:16 -0300

    feat: add env production

--- apiRoute.js
+++ apiRoute.js
@@ -4,7 +4,7 @@ const dbConnection = require("./db")
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.NODE_ENV || 3000
 
 let submittedFormData = []
 
